848fffb8b99f48224effcbf7dcadfb9c429d456a
unknown <jcamilovelandiab@gmail.com>
2019-11-12 11:27:27 -0500

    making login and launch activity

---
+++ app/src/main/java/com/example/myappandroidsecure/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.myappandroidsecure;
+
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
+}
